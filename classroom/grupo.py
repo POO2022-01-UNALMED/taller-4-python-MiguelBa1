0ca0ea7e059768eff2d86b4ce5ad929d60417330
Miguel Barrera <59462086+MiguelBa1@users.noreply.github.com>
2022-04-04 11:47:50 -0500

    prueba1

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -6,14 +6,19 @@ from classroom.asignatura import Asignatura
 
 class Grupo:
 
-    grado = None
+    grado = "Grado 12"
 
     def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def __str__(self):
+        return f"Grupo de estudiantes: {self._grupo}"
+
+    def listadoAsignaturas(self, **kwargs):
+        if self._asignaturas is None:
+            self._asignaturas = []
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
@@ -29,9 +34,10 @@ class Grupo:
         cls.grado = nombre
 
     @ classmethod
+    def asignarNombre(cls, nombre="Grado 4"):
+        cls.grado = nombre
+    
+    @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
