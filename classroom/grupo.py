7087fa0cce760ade287fa8509b73c85dcb919ef0
Miguel Barrera <59462086+MiguelBa1@users.noreply.github.com>
2022-04-04 12:00:52 -0500

    prueba3

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -8,7 +8,7 @@ class Grupo:
 
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=[]):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=[]):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
